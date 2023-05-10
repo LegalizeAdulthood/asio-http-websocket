1295c878d2756a677252f16daf64a9f70dfa19db
Richard <legalize@xmission.com>
2023-05-10 13:02:08 -0600

    Add copyright notice

--- http/server.cpp
+++ http/server.cpp
@@ -1,3 +1,19 @@
+//
+// Copyright (c) 2016-2019 Vinnie Falco (vinnie dot falco at gmail dot com)
+//
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+// Official repository: https://github.com/boostorg/beast
+//
+
+//------------------------------------------------------------------------------
+//
+// Example: HTTP flex server (plain and SSL), asynchronous
+//
+//------------------------------------------------------------------------------
+// Adapted by Richard Thomson, Utah C++ Programmers.
+
 #include <boost/beast/core.hpp>
 #include <boost/beast/http.hpp>
 #include <boost/beast/ssl.hpp>
